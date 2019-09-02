18a589da09bbc9a5e27a1f2a9273bac7578df107
Deschant Kounou <deschant.kounou@andela.com>
2019-09-03 00:16:46 +0200

    Add user interface

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Login';
+import Signup from './Signup';
+import Inbox from './Inbox';
+
 import '../assets/styles/main.scss';
+import Compose from './Compose';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/:email/inbox" component={Inbox} />
+      <Route exact path="/:email/compose" component={Compose} />
     </Switch>
   </BrowserRouter>
 );
